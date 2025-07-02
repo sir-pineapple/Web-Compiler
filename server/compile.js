eb60d4c87fb9e0348ee2285d4e37e255664f8f00
Sir Pineapple <dhruvgupta376@gmail.com>
2025-07-02 16:46:29 +0530

    Update README.md

--- server/compile.js
+++ server/compile.js
@@ -2,15 +2,35 @@ const { Router } = require('express');
 const compileRouter = Router();
 
 compileRouter.post("/c", (req, res) => {
+    const code = req.body.code;
+    if (!code) {
+        res.json({output: "Please enter code"});
+        return;
+    }
 })
 
 compileRouter.post("/cpp", (req, res) => {
+    const code = req.body.code;
+    if (!code) {
+        res.json({output: "Please enter code"});
+        return;
+    }
 })
 
 compileRouter.post("/java", (req, res) => {
+    const code = req.body.code;
+    if (!code) {
+        res.json({output: "Please enter code"});
+        return;
+    }
 })
 
 compileRouter.post("/python", (req, res) => {
+    const code = req.body.code;
+    if (!code) {
+        res.json({output: "Please enter code"});
+        return;
+    }
 })
 
 module.exports = {
