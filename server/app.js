a455a299adcfc3cdb22ef00a1c05560bd7481b17
Sir Pineapple <dhruvgupta376@gmail.com>
2025-07-02 15:32:33 +0530

    added compile router

--- server/app.js
+++ server/app.js
@@ -2,12 +2,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.post("/compile", (req, res) => {
-    const {language, code} = req.body;
-    if (!code) {
-        res.json({message: "Code must be provided"})
-    }
-})
+const { compileRouter } = require("compile");
+
+app.use("/compile", compileRouter);
 
 app.listen(3000, () => {
     console.log("listening on port 3000");
---
+++ server/compile.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+const compileRouter = Router();
+
+compileRouter.post("/c", (req, res) => {
+})
+
+compileRouter.post("/cpp", (req, res) => {
+})
+
+compileRouter.post("/java", (req, res) => {
+})
+
+compilerRouter.post("/python", (req, res) => {
+})
+
+module.exports = {
+    compileRouter: compileRouter
+}
